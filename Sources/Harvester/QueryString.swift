0d574181469d5aa4f67c87ca62b5c3470c48c8e0
Elijah Yap <yap@usc.edu>
2021-04-07 16:18:52 -0700

    include query

--- Sources/Harvester/QueryString.swift
+++ Sources/Harvester/QueryString.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public extension URL {
     init?(string: String, with parameters: [String: String]){
-        self.init(string: string
+        self.init(string: string + "?"
             + parameters
                 .map{"\($0.key)=\($0.value)"}
                 .joined(separator: "&")
