f5e08bebc3596439f8e03bd7862426ddc8d0b7e8
Elijah Yap <yap@usc.edu>
2021-04-07 14:22:15 -0700

    fix?

--- Sources/Harvester/Harvester.swift
+++ Sources/Harvester/Harvester.swift
@@ -4,22 +4,6 @@ struct Harvester {
     var text = "Hello, World!"
 }
 
-#if canImport(Combine)
-import Combine
-
-@available(iOS 13.0, *)
-let dataTaskMonitor = { (result: URLSession.DataTaskPublisher.Output) -> Data in
-    let code = (result.response as? HTTPURLResponse)?.statusCode
-        ?? -1
-    if !(200...299).contains(code) {
-        print(result.response)
-        print("HTTP Error with Code: \(code)")
-    }
-    return result.data
-}
-
-#endif
-
 extension Sequence {
     public func random(_ num: Int) -> [Element] {
         Array(shuffled().prefix(num))
