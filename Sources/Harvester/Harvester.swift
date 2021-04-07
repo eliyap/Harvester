7296d332b88adc67dce2524fd56c12b67aa0f0ac
Elijah Yap <yap@usc.edu>
2021-04-07 14:06:12 -0700

    copy over

--- Sources/Harvester/Harvester.swift
+++ Sources/Harvester/Harvester.swift
@@ -1,3 +1,21 @@
+import Foundation
+
 struct Harvester {
     var text = "Hello, World!"
 }
+
+#if canImport(Combine)
+import Combine
+
+@available(iOS 13.0, *)
+let dataTaskMonitor = { (result: URLSession.DataTaskPublisher.Output) -> Data in
+    let code = (result.response as? HTTPURLResponse)?.statusCode
+        ?? -1
+    if !(200...299).contains(code) {
+        print(result.response)
+        print("HTTP Error with Code: \(code)")
+    }
+    return result.data
+}
+
+#endif
